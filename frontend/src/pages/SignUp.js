adef50f5bd29a654c6405563c1d9962e7d9eb4ef
Aayushbahadurpradhan <pradhanaayush9841@gmail.com>
2024-08-05 21:47:35 +0545

    updated signup change

--- frontend/src/pages/SignUp.js
+++ frontend/src/pages/SignUp.js
@@ -266,6 +266,28 @@ const SignUp = () => {
         }
     };
 
+    return (
+        <section id='signup'>
+            <div className='mx-auto container p-4'>
+                <div className='bg-white p-5 w-full max-w-sm mx-auto'>
+                    <div className='w-20 h-20 mx-auto relative overflow-hidden rounded-full'>
+                        <div>
+                            <img src={data.profilePic || loginIcons} alt='Profile' />
+                        </div>
+                        <form>
+                            <label>
+                                <div className='text-xs bg-opacity-80 bg-slate-200 pb-4 pt-2 cursor-pointer text-center absolute bottom-0 w-full'>
+                                    Upload Photo
+                                </div>
+                                <input type='file' className='hidden' onChange={handleUploadPic} />
+                            </label>
+                        </form>
+                    </div>
+
+                </div>
+            </div>
+        </section>
+    );
 };
 
 export default SignUp;
